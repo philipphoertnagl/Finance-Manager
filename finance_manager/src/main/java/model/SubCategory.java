a37902951befdefb8940fbe22a195def6bd0e3b7
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-02-02 15:02:52 +0100

    add SimpleString Properties for using in TableView

--- finance_manager/src/main/java/model/SubCategory.java
+++ finance_manager/src/main/java/model/SubCategory.java
@@ -2,24 +2,41 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class SubCategory {
-    private final String subName;
-    private double amount;
+    private StringProperty subName;
+    private DoubleProperty amount;
     private List<Transaction> transactions;
 
     public SubCategory(String name) {
-        this.subName = name;
+        this.subName = new SimpleStringProperty(name);
+        this.amount = new SimpleDoubleProperty(0);
         this.transactions = new ArrayList<>();
     }
 
     public void addTransaction(Transaction transaction) {
         transactions.add(transaction);
-        this.amount += transaction.getAmount();
+        this.amount.set(this.amount.get() + transaction.getAmount());
     }
 
     public double getAmount() {
+        return amount.get();
+    }
+
+    public DoubleProperty amountProperty() {
         return amount;
+    }
+
+    public String getSubName() {
+        return subName.get();
+    }
+
+    public StringProperty subNameProperty() {
+        return subName;
     }
 
     @Override
