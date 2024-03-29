a0b08acd272df755a212549410f04c26df2f54d8
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-03-29 14:39:27 +0100

    added "prettyPrinting" when generating a new JSON file mit objectMapper.enable(SerializationFeature.INDENT_OUTPUT);

--- finance_manager/src/main/java/service/DataStorage.java
+++ finance_manager/src/main/java/service/DataStorage.java
@@ -1,6 +1,7 @@
 package service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import model.DataModel;
 
 import java.io.File;
@@ -10,9 +11,14 @@ public class DataStorage {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    public DataStorage() {
+        // Enable pretty printing
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
     public void saveDataModel(DataModel dataModel, String filePath) {
         try {
             objectMapper.writeValue(new File(filePath), dataModel);
+            System.out.println("DataModel saved to JSON: " + filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
