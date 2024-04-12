7b6d187bbba0e8fce1436c9a01c369b727f7fe5a
phl <ic22b116@technikum-wien.at>
2024-04-13 01:06:16 +0200

    trying to add new lines in datasheet, with date automatically set. Problem: transaction name not setable

--- finance_manager/src/main/java/controller/TransactionEditController.java
+++ finance_manager/src/main/java/controller/TransactionEditController.java
@@ -2,20 +2,68 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
-import model.Category;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.NumberStringConverter;
 import model.DataModel;
+import model.Transaction;
 import model.SubCategory;
-import service.DataStorage;
 
-import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class TransactionEditController {
+    private DataModel dataModel = new DataModel();
+    private SubCategory subCategory;
 
+    @FXML
+    private TableView<Transaction> tableviewCategory;
+    @FXML
+    private TableColumn<Transaction, String> tableColumnTransaction;
+    @FXML
+    private TableColumn<Transaction, Number> tableColumnAmount;
+    @FXML
+    private TableColumn<Transaction, String> tableColumnDate;
+
+    @FXML
+    public void initialize() {
+        setupTableColumns();
+    }
+
+    private void setupTableColumns() {
+        tableColumnTransaction.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        tableColumnTransaction.setCellFactory(TextFieldTableCell.forTableColumn());
+        tableColumnTransaction.setOnEditCommit(event -> {
+            Transaction transaction = event.getRowValue();
+            transaction.setName(event.getNewValue());
+
+        });
+
+        tableColumnAmount.setCellValueFactory(cellData -> cellData.getValue().amountProperty());
+        tableColumnAmount.setCellFactory(TextFieldTableCell.forTableColumn(new NumberStringConverter()));
+        tableColumnAmount.setOnEditCommit(event -> {
+            Transaction transaction = event.getRowValue();
+            transaction.setAmount(event.getNewValue().doubleValue());
+            transaction.setDate(LocalDateTime.now());  // Set the current date when the amount is edited
+        });
+
+        tableColumnDate.setCellValueFactory(cellData -> cellData.getValue().dateProperty().asString());
+        tableColumnDate.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        tableviewCategory.setEditable(true);
+        tableColumnTransaction.setEditable(true);
+    }
+
+    @FXML
+    private void handleAddButtonAction(ActionEvent event) {
+        Transaction newTransaction = new Transaction(0); // Start with an amount of 0
+        newTransaction.setDate(LocalDateTime.now());  // Set the current date when adding a new transaction
+        tableviewCategory.getItems().add(newTransaction);
+        subCategory.addTransaction(newTransaction);
+
+        int newRowIndex = tableviewCategory.getItems().size() - 1;
+        tableviewCategory.getSelectionModel().select(newRowIndex);
+        tableviewCategory.scrollTo(newTransaction);
+        tableviewCategory.edit(newRowIndex, tableColumnTransaction);
+    }
 }
--- finance_manager/src/main/java/model/Transaction.java
+++ finance_manager/src/main/java/model/Transaction.java
@@ -1,33 +1,73 @@
 package model;
 
+import javafx.beans.property.*;
+
 import java.time.LocalDateTime;
 
 public class Transaction {
 
-    private double amount;
-    private LocalDateTime date;
-    private String type;
+    private DoubleProperty amount;
+    private ObjectProperty<LocalDateTime> date;
+    private StringProperty name;
 
 
     public Transaction(double amount) {
-        this.amount = amount;
-        this.date = LocalDateTime.now();
+        this.amount = new SimpleDoubleProperty(amount);
+        this.date = new SimpleObjectProperty<>(LocalDateTime.now());
     }
+
+    public Transaction(String name) {
+        this.name = new SimpleStringProperty(name);
+        this.amount = new SimpleDoubleProperty(0);
+        this.date = new SimpleObjectProperty<>(LocalDateTime.now());
+    }
+
+
     public Transaction() {
 
     }
 
     public double getAmount() {
+        return amount.get();
+    }
+
+    public void editAmount(double amountToAdd) {
+        double currentAmount = this.amount.get(); // Get the current amount
+        this.amount.set(currentAmount + amountToAdd); // Set the new amount
+        System.out.println("We transferred " + amountToAdd + " to the account at " + date);
+    }
+
+    public DoubleProperty amountProperty() {
         return amount;
     }
 
-    public void editAmount(double amount) {
-        this.amount += amount;
-        System.out.println("We transferred " + amount + " to the account at " + date);
-
+    public void setAmount(double amount) {
+        this.amount.set(amount);
     }
 
+    public LocalDateTime getDate() {
+        return date.get();
+    }
 
+    public ObjectProperty<LocalDateTime> dateProperty() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date.set(date);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
 
     @Override
     public String toString() {
