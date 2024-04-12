7e9dd999632c2dee3fc6d507dac5a30d6991ffb2
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-04-12 23:21:00 +0200

    add TransactionEditController + View

--- finance_manager/src/main/java/controller/TransactionEditController.java
+++ finance_manager/src/main/java/controller/TransactionEditController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import model.Category;
+import model.DataModel;
+import model.SubCategory;
+import service.DataStorage;
+
+import java.io.IOException;
+
 public class TransactionEditController {
+
 }
